97f0e583e9f17262d70d4b63792930006ce64b03
ArameBarre <128553945+ArameBarre@users.noreply.github.com>
2023-04-07 12:23:16 -0400

    Ajout-dela-classe-Hotel

---
+++ Hotel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GestiondesClientsdansunHotel.Gestion_Client_Hotel
+{
+    internal class Hotel
+    {
+    }
+}
